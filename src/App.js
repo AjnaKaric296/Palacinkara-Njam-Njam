f7316a86facdd8bb9ed4d5d11f2b55595bae0d0d
Ajna Karic <ajna.karic.24@size.ba>
2025-05-27 00:54:51 +0200

    test

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ export default function App() {
             <li><a href="#menu">O NAMA</a></li>
             <li><a href="#gallery">POSLASTICE</a></li>
             <li><a href="#contact">KONTAKT</a></li>
-            <li><a href="#contact">LOKACIJA</a></li>
+            <li><a href="#contact">LOKACIJa</a></li>
           </ul>
         </nav>
         <div className="hero">
           
 
 
-          
+
         </div>
       </header>
 
